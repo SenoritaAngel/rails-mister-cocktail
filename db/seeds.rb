d185b73b6668b755cffb606c8c3cf3089d768847
Isabelle Baert <webrelationpro@gmail.com>
2019-02-14 14:21:51 +0100

    new model cocktail add

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "pry-byebug"
+require "json"
+
+ingredients = List.get "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+ingredients.each do |ingredient|
+  cocktails = List.get "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=#{ingredient}"
+  cocktail = JSON.parse(cocktails)
+
+  cocktail = Cocktail.new(name: cocktail['strIngredient1'])
+
+  cocktail.save!
+end
+# cocktails = Cocktail.create({name: "Mojito"}, {name: "Margarita"}, {name: "Tequila Sunrise"})
+# ingredients = Ingredient.create({name: "lemon"}, {name: "ice"}, {name: "mint leaves"})
